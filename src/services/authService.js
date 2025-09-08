6bc609be686d136337f7931c36675de3da971937
User Name <landonisaiah@gmail.com>
2025-09-08 00:30:53 -0700

    add sign in functionality

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,12 +4,12 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`;
 
 const signUp = async (formData) => {
     try {
-        const response = await axios.post(`${BASE_URL}/sign-up`, {
+        const res = await axios.post(`${BASE_URL}/sign-up`, {
             screen_name: formData.screen_name,
             password: formData.password
         });
 
-        const data = response.data;
+        const data = res.data;
 
         if (data.token) {
             localStorage.setItem('token', data.token);
@@ -19,13 +19,38 @@ const signUp = async (formData) => {
         throw new Error('No token received from server');
     } catch (err) {
         console.error('Sign up error:', err);
-        if (err.response && err.response.data && err.response.data.err) {
-            throw new Error(err.response.data.err);
+        if (err.res && err.res.data && err.res.data.err) {
+            throw new Error(err.res.data.err);
         }
-        throw new Error('Network error - please try again');
+        throw new Error('Network error, please try again');
+    }
+};
+
+const signIn = async (formData) => {
+    try {
+        const res = await axios.post(`${BASE_URL}/sign-in`, {
+            screen_name: formData.screen_name,
+            password: formData.password
+        });
+
+        const data = res.data;
+
+        if (data.token) {
+            localStorage.setItem('token', data.token);
+            return JSON.parse(atob(data.token.split('.')[1])).payload;
+        }
+
+        throw new Error('No token received from server');
+    } catch (err) {
+        console.error('Sign in error:', err);
+        if (err.res && err.res.data && err.res.data.err) {
+            throw new Error(err.res.data.err);
+        }
+        throw new Error('Netword error, please try again');
     }
 };
 
 export {
     signUp,
+    signIn,
 };
